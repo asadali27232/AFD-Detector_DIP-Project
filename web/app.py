0b8c404e12bf839fd47d21995acf840dec248e7e
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-11-22 16:44:57 +0500

    Updated

--- web/app.py
+++ web/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -9,6 +10,26 @@ def home():
 @app.route('/detector')
 def detector():
     return render_template('detector.html')
+
+@app.route('/detector/upload', methods=['POST'])
+def upload_file():
+    file = request.files['file']
+    # Check if the file is present and allowed
+    if file and allowed_file(file.filename):
+        # Save the file with a new name and JPG extension
+        filename = 'uploaded_image.jpg'
+        filepath = os.path.join(filename)
+        file.save(filepath)
+
+        # Return a success message
+        return 'File uploaded and saved as JPG successfully.'
+
+    return 'Invalid file.'
+
+def allowed_file(filename):
+    # You can add more file extensions if needed
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'png', 'jpg', 'jpeg', 'gif'}
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ web/static/js/detector_script.js
@@ -0,0 +1,48 @@
+// Function to handle the image upload
+function uploadImage(event) {
+  // Prevent default action
+
+  // Create a file input element
+  const input = document.createElement('input');
+  input.type = 'file';
+  input.accept = 'image/*'; // Accept all image types
+
+  // Handle file selection
+  input.onchange = (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+
+    // Set the image as the background of the div
+    const reader = new FileReader();
+    reader.onload = function (readEvent) {
+      document.getElementById(
+        'upload-image'
+      ).style.backgroundImage = `url(${readEvent.target.result})`;
+    };
+    reader.readAsDataURL(file);
+
+    // Send the file to the server
+    sendFileToServer(file);
+  };
+
+  // Trigger the file input dialog
+  input.click();
+}
+
+// Function to send the file to the server
+function sendFileToServer(file) {
+  const formData = new FormData();
+  formData.append('file', file);
+
+  fetch('/detector/upload', {
+    method: 'POST',
+    body: formData,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
