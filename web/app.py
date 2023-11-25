fdc07db8b528106dab77f672d576d05ba0d5ff24
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-11-25 18:12:11 +0500

    Updated

--- web/app.py
+++ web/app.py
@@ -10,6 +10,34 @@ CORS(app)
 CORS(app, origins=["http://localhost:19006"])
 
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/detector')
+def detector():
+    return render_template('detector.html')
+
+
+@app.route('/detector/upload', methods=['POST'])
+def upload_file():
+    file = request.files['file']
+    # Check if the file is present and allowed
+    if file and model.allowed_file(file.filename):
+        # Save the file with a new name and JPG extension
+        filename = 'uploaded_image.jpg'
+        filepath = os.path.join(filename)
+        file.save(filepath)
+
+        # # Make prediction
+        # predicted_class = model.predict(filepath)
+        # return json.dumps({'predicted_class': predicted_class})
+        return jsonify({'filename': filepath})
+
+    return jsonify({'inv': 'Invalid file.'})
+
+
 @app.route('/upload', methods=['POST'])
 def upload_image():
     print('upload_image')
@@ -42,34 +70,6 @@ def upload_image():
 
     except Exception as e:
         return jsonify({'error': str(e)})
-
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-
-@app.route('/detector')
-def detector():
-    return render_template('detector.html')
-
-
-@app.route('/detector/upload', methods=['POST'])
-def upload_file():
-    file = request.files['file']
-    # Check if the file is present and allowed
-    if file and model.allowed_file(file.filename):
-        # Save the file with a new name and JPG extension
-        filename = 'uploaded_image.jpg'
-        filepath = os.path.join(filename)
-        file.save(filepath)
-
-        # # Make prediction
-        # predicted_class = model.predict(filepath)
-        # return json.dumps({'predicted_class': predicted_class})
-        return jsonify({'filename': filepath})
-
-    return jsonify({'inv': 'Invalid file.'})
 
 
 if __name__ == '__main__':
