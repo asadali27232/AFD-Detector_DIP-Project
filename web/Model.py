3a29acc986aa66feb766f33ca4e4108cc45a58e6
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-11-25 13:51:28 +0500

    Updated

---
+++ web/Model.py
@@ -0,0 +1,33 @@
+from tensorflow.keras.models import load_model
+import numpy as np
+import cv2 as cv2
+
+
+class Model:
+    def allowed_file(filename):
+        # You can add more file extensions if needed
+        return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'png', 'jpg', 'jpeg', 'gif'}
+
+    def predict(img_path):
+        model = load_model('afd.h5')
+        class_labels = ['complex', 'frog_eye_leaf_spot',
+                        'healthy', 'powdery_mildew', 'rust', 'scab']
+
+        # Load and preprocess the input image using OpenCV
+        img = cv2.imread(img_path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        img = cv2.resize(img, (224, 224))
+        img_array = np.expand_dims(img, axis=0)
+
+        # Make predictions
+        prediction = model.predict(img_array)
+
+        # Dsiplay Results
+        prediction = np.array(prediction)
+        prediction = np.argmax(prediction, axis=1)
+        prediction = list(prediction)
+
+        predicted_class = class_labels[prediction[0]]
+
+        return predicted_class
--- web/app.py
+++ web/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, jsonify
-from tensorflow.keras.models import load_model
 from flask_cors import CORS
-import numpy as np
+import Model as model
 import json
 import cv2 as cv2
 import os
@@ -25,47 +24,18 @@ def detector():
 def upload_file():
     file = request.files['file']
     # Check if the file is present and allowed
-    if file and allowed_file(file.filename):
+    if file and model.allowed_file(file.filename):
         # Save the file with a new name and JPG extension
         filename = 'uploaded_image.jpg'
         filepath = os.path.join(filename)
         file.save(filepath)
 
-        # Make prediction
-        predicted_class = predict(filepath)
-        return json.dumps({'predicted_class': predicted_class})
+        # # Make prediction
+        # predicted_class = model.predict(filepath)
+        # return json.dumps({'predicted_class': predicted_class})
+        return json.dumps({'filepath': filepath})
 
-    return 'Invalid file.'
-
-
-def allowed_file(filename):
-    # You can add more file extensions if needed
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'png', 'jpg', 'jpeg', 'gif'}
-
-
-def predict(img_path):
-    model = load_model('afd.h5')
-    class_labels = ['complex', 'frog_eye_leaf_spot',
-                    'healthy', 'powdery_mildew', 'rust', 'scab']
-
-    # Load and preprocess the input image using OpenCV
-    img = cv2.imread(img_path)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-    img = cv2.resize(img, (224, 224))
-    img_array = np.expand_dims(img, axis=0)
-
-    # Make predictions
-    prediction = model.predict(img_array)
-
-    # Dsiplay Results
-    prediction = np.array(prediction)
-    prediction = np.argmax(prediction, axis=1)
-    prediction = list(prediction)
-
-    predicted_class = class_labels[prediction[0]]
-
-    return predicted_class
+    return json.dumps({'inv': 'Invalid file.'})
 
 
 if __name__ == '__main__':
